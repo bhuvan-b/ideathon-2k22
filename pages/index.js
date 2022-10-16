292193da8e6c0ddade7d081b007cf9bbb8a39c7e
Kanishka <kanishka.bansode@gmail.com>
2022-10-16 23:38:48 +0530

    speakers section blueprint

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import SpeakerSection from "../components/SpeakerSection";
+import SpeakerSection from "../components/Speaker Section/SpeakerSection";
 import LandingSection from "../components/LandingSection";
 
 export default function Home() {
   return (
     <div className="">
       <LandingSection />
+      <SpeakerSection/>
     </div>
   );
 }
