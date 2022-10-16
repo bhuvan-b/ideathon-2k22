c9bacb3deb18beaa8b242fbc080b0ec72876bc1f
Kanishka <kanishka.bansode@gmail.com>
2022-10-16 17:47:33 +0530

    bg image uploaded and added on the home page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:[
+      'imgur.com',
+      "i.imgur.com"
+  ]
+  },
   swcMinify: true,
 }
 
