f4845aaaef7785ddcd9a433726f435b24fa3069a
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-10-16 18:18:26 +0530

    Heading Component added

---
+++ components/SubHeading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const SubHeading = (props) =>  {
+  return (
+    <div className='subhead'>
+      <div>{props.head}</div>
+    </div>
+  )
+}
+
+export default SubHeading;
