e11e8747b4c55546b6408842ab959d9aa01b0b16
bhuvan-b <bokkabhuvan2@gmail.com>
2022-10-18 03:40:14 +0530

    Landing Page And Body Changes

--- components/LandingSection.jsx
+++ components/LandingSection.jsx
@@ -6,19 +6,19 @@ const LandingSection = () => {
   return (
     <div className="">
       <div>
-        <h3 className="text-3xl text-[#432D1D] font-gilroyBold ml-[27rem] -mb-10 mt-10">
+        {/* <h3 className="text-3xl text-[#432D1D] font-gilroyBold ml-[27rem] -mb-10 mt-10">
           RELIVING THE PAST
-        </h3>
+        </h3> */}
         <SubHeading type="mainhead" head="IDEATHON"></SubHeading>
-        <h3 className="text-3xl text-right text-[#432D1D] font-gilroyBold mr-[27rem] -mt-28">
+        {/* <h3 className="text-3xl text-right text-[#432D1D] font-gilroyBold mr-[27rem] -mt-28">
           2K22
-        </h3>
+        </h3> */}
         {/* <h3 className="text-4xl text-[#432D1D] font-gilroyBold">
           REMINISCE TO IDEATE
         </h3> */}
       </div>
-      <div className=" -mt-20">
-        <img className="" src="https://i.imgur.com/OSxeX43.png" alt="" />
+      <div className="mt-8">
+        <img className="" src="https://i.imgur.com/trZs8Ty.png" alt="" />
       </div>
     </div>
   );
