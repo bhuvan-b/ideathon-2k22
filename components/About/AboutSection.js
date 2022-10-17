345e382d49123ff02aa402caf5273bd2d96f7865
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-10-17 10:49:57 +0530

    Subheading mediarule updated

--- components/About/AboutSection.js
+++ components/About/AboutSection.js
@@ -5,7 +5,7 @@ function AboutSection() {
   return (
     
     <div className='content-center'>
-      <SubHeading head="ABOUT IDEATHON '22"/>
+      <div><SubHeading head="ABOUT IDEATHON '22"/></div>
       <div className=' text-justify mx-20 mt-3 text-xl'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis quisque eu phasellus cum. Morbi nec massa convallis et risus diam, aliquam. Gravida scelerisque quisque donec aliquam enim ultrices pulvinar. Purus vitae eu id habitant velit. Erat dui, vel sed vel. Mollis nunc sed facilisis sed vitae id sit euismod feugiat. Proin et nulla enim, porta metus turpis quam sed nulla. Lorem eros, egestas volutpat neque, tempor adipiscing magna. Dui tellus.
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis quisque eu phasellus cum. Morbi nec massa convallis et risus diam, aliquam. Gravida scelerisque quisque donec aliquam enim ultrices pulvinar. Purus vitae eu id habitant velit. Erat dui, vel sed vel. Mollis nunc sed facilisis sed vitae id sit euismod feugiat. Proin et nulla enim, porta metus turpis quam sed nulla. Lorem eros, egestas volutpat neque, tempor adipiscing magna. Dui tellus.
--- components/SubHeading.js
+++ components/SubHeading.js
@@ -1,17 +1,17 @@
 import React from 'react'
 const SubHeading = (props) =>  {
   return (
-    <>
+    <div className='subheadparent'>
         <div className='subhead1'>
-          <div className=' text-xl'>{props.head}</div>
+          <div className=' text-6xl fontsize'>{props.head}</div>
         </div>
         <div className='subhead2'>
-            <div className=' text-xl'>{props.head}</div>
+            <div className=' text-6xl fontsize'>{props.head}</div>
         </div>
         <div className='subhead3'>
-            <div className=' text-xl'>{props.head}</div>
+            <div className=' text-6xl fontsize'>{props.head}</div>
         </div>
-    </>
+    </div>
     
   )
 }
