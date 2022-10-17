25870654f1a8cc51c96f4f2eba022a3ee2558235
agarwalvinit5678 <92209640+agarwalvinit5678@users.noreply.github.com>
2022-10-17 11:04:42 +0530

    outline added to subheads

--- components/SubHeading.js
+++ components/SubHeading.js
@@ -3,7 +3,7 @@ const SubHeading = (props) =>  {
   return (
     <div className='subheadparent'>
         <div className='subhead1'>
-          <div className=' text-6xl fontsize'>{props.head}</div>
+          <div className=' text-6xl fontsize outlinetext'>{props.head}</div>
         </div>
         <div className='subhead2'>
             <div className=' text-6xl fontsize'>{props.head}</div>
