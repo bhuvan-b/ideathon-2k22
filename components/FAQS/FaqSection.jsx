f6ba9c470a126c944079157e2b511c9fca1bbff0
som <somsinghlodhi2323@gmail.com>
2022-10-18 02:36:05 +0530

    changed 4

--- components/FAQS/FaqSection.jsx
+++ components/FAQS/FaqSection.jsx
@@ -8,11 +8,11 @@ export default function FaqSection() {
 
   return (
     <div className="p-4 flex flex-col justify-center items-center">
-      <div className="sm:w-3/4 w-[85%] bg-[#000A0C]" id="faqs">
+      <div className="sm:w-3/4 w-[100%] bg-[#FDEC0]" id="faqs">
         <div className="p-2 pb-14 text-center font-Montserrat font-bold text-4xl md:text-6xl flex flex-col items-center justify-center">
           <div>
-            <span className="text-white">FAQ</span>
-            <span className="text-cyan-400">S </span>
+            <span className="text-#264653">FAQ</span>
+            <span className="text-#264653">S </span>
           </div>
           <hr className="secheadingfaq" />
         </div>
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import SpeakerSection from "../components/Speaker Section/SpeakerSection";
 import LandingSection from "../components/LandingSection";
-
+import AboutSection from "../components/About/AboutSection";
+import FaqSection from "../components/FAQS/FaqSection";
 export default function Home() {
   return (
     <div className="">
       <LandingSection />
+      <AboutSection />
+      <SpeakerSection />
+      <FaqSection />
     </div>
   );
 }
