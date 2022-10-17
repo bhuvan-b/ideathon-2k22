3528fb2190d8f5ed945c527600924700ae88c0ec
som <somsinghlodhi2323@gmail.com>
2022-10-17 10:39:40 +0530

    changes 2

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import SpeakerSection from "../components/SpeakerSection";
-import FaqSection from "../components/FAQS/FaqSection";
-
-export default function Home() {
-  return (
-    <div>
-      <Image
-        className="-z-10"
-        src={"https://i.imgur.com/AvmKsjU.jpg"}
-        alt={""}
-        layout="fill"
-      />
-      <FaqSection />
-=======
 import LandingSection from "../components/LandingSection";
+import FaqSection from "../components/FAQS/FaqSection";
 
 export default function Home() {
   return (
     <div className="">
       <LandingSection />
-
+      <FaqSection />
     </div>
   );
 }
