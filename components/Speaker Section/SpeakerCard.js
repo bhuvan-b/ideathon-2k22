64049e3822da2b2bf597f4c3b5bc5f9c713978b7
Kanishka <kanishka.bansode@gmail.com>
2022-10-18 14:29:17 +0530

    added the suggested improvements to the speaker section

--- components/Speaker Section/SpeakerCard.js
+++ components/Speaker Section/SpeakerCard.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 function SpeakerCard({name , picture , Designation , Company}) {
   return (
-    <div className='w-1/6 h-1/4 flex justify-center items-center flex-col bg-white m-[6vmax] py-[1vmax] ticket'>
-        <img className='w-[14vmax]' src={picture} />
-        <div className='flex mx-[5%] my-[3%] flex-col p-[10%]  justify-center items-center border-dashed border-[1.2px] border-[#D9D9D9] ticket-inner'>
+    <div className='w-1/6 h-1/4 flex justify-center items-center flex-col bg-white lg:m-[6vmax] sm:mx-[20vmax]  sm:my-[3vmax] px-0 pt-[0.5vmax] pb-[1vmax] ticket'>
+        <img className='w-[93%]' src={picture} />
+        <div className='flex lg:w-[93%] -z-10 sm:h-[60%] sm:w-[60%] h-[85%] mx-[5%] my-[5%] flex-col px-[5%] py-[3%]  justify-center items-center border-dashed border-[1.2px] border-[#9F8772] ticket-inner'>
             <h1 className='text-[1.3vmax] text-[#542436] font-["Consolas"] font-[700]'>{name}</h1>
             <div className='flex justify-center items-center flex-col'>
                 <h3 className='text-[1.1vmax] flex justify-center items-center font-[400] text-[#363636]'>{Designation}</h3>
                 <h3 className='text-[1vmax] flex justify-center items-center font-[400] text-[#363636]'>{Company}</h3>
-
             </div>
             
         </div>
