eb303f62aff326bfa62cd29b9917862f216eedd2
pranav11-02 <pranav.more@eubrics.com>
2022-10-31 19:46:05 +0530

    cors new

--- next.config.js
+++ next.config.js
@@ -25,6 +25,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
+      },
+    ];
+  },
   swcMinify: true,
 };
 
