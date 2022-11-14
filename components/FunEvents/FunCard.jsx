ab37fcc1ba718761335564e2b91be78eb666ddae
pranav11-02 <pranav.more@eubrics.com>
2022-11-14 22:42:46 +0530

    Side events page added

---
+++ components/FunEvents/FunCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FunCard = () => {
+  return (
+    <div className="pt-8">
+    <div className="hidden md:block px-8 py-2 lg:p-12 mt-6">
+        <img className="carImg" src="https://i.imgur.com/Bg0huXX.jpg"  alt="" />
+      </div>
+
+      <div className="mt-6  md:hidden">
+        <img className="carImg" src="https://i.ibb.co/F7vsXy1/Stand-sse.webp" alt="" />
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default FunCard;
